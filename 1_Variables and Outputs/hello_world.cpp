b5669922990cbbbf1e1df82f2ab3cc6bf860fdf4
Sudhanshu Barnwal <sudhanshub810@gmail.com>
2023-11-30 16:19:40 +0530

    C++ With DSA

--- 1_Variables and Outputs/hello_world.cpp
+++ 1_Variables and Outputs/hello_world.cpp
@@ -8,6 +8,6 @@ using namespace std;
 int main(){
 
     // Prints hello world in c++
-    cout << "\n Sudhanshu Barnwal in C++." << endl;
+    cout << "\n Hello World in C++." << endl;
     return 0;
 }
